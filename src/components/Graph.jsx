9f0cfb13d6d405ac0818b0cfa7923c1df1013a3e
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-11-26 21:39:25 +0530

    Update the header component

---
+++ src/components/Graph.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Graph() {
+  return (
+    <div>
+        <div>
+              <p>This is the graph section </p>
+        </div>
+    </div>
+  )
+}
+
+export default Graph
---
+++ src/components/Reminder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Reminder() {
+  return (
+    <div>
+        <div>
+            <p>this is the reminder section </p>
+        </div>
+    </div>
+  )
+}
+
+export default Reminder
