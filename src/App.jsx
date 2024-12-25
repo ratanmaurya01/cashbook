2b8c46ae7bf164f0c8b6930f08df1aafdb0776d6
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-25 09:17:11 +0530

    Update the header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"; // Only import Routes and Route
 import Navbar from "./Navbar/Navbar";
 import Service from "./Service/Service";
-import Home from './Home/Home'
 import Cashbook from "./Home/Cashbook";
-import NotificationButton from "./components/Notification";
 import Mainpage from "./components/Mainpage";
 import Todo from "./Todo/Todo";
 import ActivityTracker from "./components/ActivityTracker";
