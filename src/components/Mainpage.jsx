a9740fc2ee3cdfc471226d5c71b7b3141e8f4236
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-29 20:17:58 +0530

    Update the header component

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -50,6 +50,13 @@ function Mainpage() {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Noteworthy technology acquisitions 2021
             </h5>
+            <div>
+              <div>
+                <div>
+                  
+                </div>
+              </div>
+            </div>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
             </p>
