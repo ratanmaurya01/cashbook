98863a94bc056db97bc928c920c1a5020565c5d8
ratanmaurya01 <ratn.maxwell@gmail.com>
2025-01-03 21:03:48 +0530

    Update the header component

--- src/Home/Cashbook.jsx
+++ src/Home/Cashbook.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import '../animate.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import DatePicker from 'react-datepicker';
